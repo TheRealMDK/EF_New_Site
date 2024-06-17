75af0f055b5785c8a5e0d1d23ae6c01e56844b18
Clinton <devbyclinton@gmail.com>
2024-06-17 11:01:13 +0200

    update form submission - still WIP

--- server.py
+++ server.py
@@ -1,7 +1,4 @@
-from flask import Flask, render_template, flash, redirect, url_for
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Email
+from flask import Flask, render_template, request
 import smtplib
 import os
 from dotenv import load_dotenv
@@ -20,6 +17,40 @@ app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 # Define the route for the home page
 @app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        user_subject = request.form.get("subject")
+        message = request.form.get("message")
+
+        # Email sending logic using environment variables
+        smtp_server = os.environ.get("MG_SMTP_SERVER")
+        smtp_username = os.environ.get("MG_SMTP_USERNAME")
+        smtp_password = os.environ.get("MG_SMTP_PASSWORD")
+        port = 587
+        sender = "edenfitness4@gmail.com"
+        receiver = "info@edenfitness.co.za"
+
+        # Construct the email message
+        subject = f"New email enquiry received."
+        body = ""
+        body += (f"Name: {name}\n"
+                 f"Email: {email}\n"
+                 f"Subject: {user_subject}\n"
+                 f"Message: {message}"
+                 )
+        message = f"Subject: {subject}\nTo: {receiver}\nFrom: {sender}\n\n{body}"
+        message = message.encode("utf-8")
+
+        # Send the email
+        try:    
+            with smtplib.SMTP(smtp_server, port) as server:
+                server.starttls()
+                server.login(smtp_username, smtp_password)
+                server.sendmail(sender, receiver, message)
+        except Exception as e:
+            print(f"Error sending email: {str(e)}")
+
     return render_template("index.html")
 
 
