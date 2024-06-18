26cbe8eb0e67bc7a3261fe2d095504172e779e92
TheRealMDK <clintonoliver25@gmail.com>
2024-06-18 17:03:45 +0200

    refactor file formatting for better readibiity

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -1,25 +1,23 @@
 (function ($) {
-	
-	"use strict";
+    'use strict';
 
-	$(function() {
-        $("#tabs").tabs();
+    $(function () {
+        $('#tabs').tabs();
     });
 
-	$(window).scroll(function() {
-	  var scroll = $(window).scrollTop();
-	  var box = $('.header-text').height();
-	  var header = $('header').height();
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        var box = $('.header-text').height();
+        var header = $('header').height();
 
-	  if (scroll >= box - header) {
-	    $("header").addClass("background-header");
-	  } else {
-	    $("header").removeClass("background-header");
-	  }
-	});
-	
+        if (scroll >= box - header) {
+            $('header').addClass('background-header');
+        } else {
+            $('header').removeClass('background-header');
+        }
+    });
 
-	$('.schedule-filter li').on('click', function() {
+    $('.schedule-filter li').on('click', function () {
         var tsfilter = $(this).data('tsfilter');
         $('.schedule-filter li').removeClass('active');
         $(this).addClass('active');
@@ -27,89 +25,90 @@
         $('.schedule-image').attr('src', imagePath);
     });
 
+    // Window Resize Mobile Menu Fix
+    mobileNav();
 
-	// Window Resize Mobile Menu Fix
-	mobileNav();
+    // Scroll animation init
+    window.sr = new scrollReveal();
 
+    // Menu Dropdown Toggle
+    if ($('.menu-trigger').length) {
+        $('.menu-trigger').on('click', function () {
+            $(this).toggleClass('active');
+            $('.header-area .nav').slideToggle(200);
+        });
+    }
 
-	// Scroll animation init
-	window.sr = new scrollReveal();
-	
+    $(document).ready(function () {
+        $(document).on('scroll', onScroll);
 
-	// Menu Dropdown Toggle
-	if($('.menu-trigger').length){
-		$(".menu-trigger").on('click', function() {	
-			$(this).toggleClass('active');
-			$('.header-area .nav').slideToggle(200);
-		});
-	}
+        //smoothscroll
+        $('.scroll-to-section a[href^="#"], a.logo[href^="#"]').on(
+            'click',
+            function (e) {
+                e.preventDefault();
+                $(document).off('scroll');
 
+                $('a').each(function () {
+                    $(this).removeClass('active');
+                });
+                $(this).addClass('active');
 
-	$(document).ready(function () {
-	    $(document).on("scroll", onScroll);
-	    
-	    //smoothscroll
-	    $('.scroll-to-section a[href^="#"], a.logo[href^="#"]').on('click', function (e) {
-	        e.preventDefault();
-	        $(document).off("scroll");
-	        
-	        $('a').each(function () {
-	            $(this).removeClass('active');
-	        })
-	        $(this).addClass('active');
-	      
-	        var target = this.hash,
-	        menu = target;
-	       	var target = $(this.hash);
-	        $('html, body').stop().animate({
-	            scrollTop: (target.offset().top) + 1
-	        }, 500, 'swing', function () {
-	            window.location.hash = target;
-	            $(document).on("scroll", onScroll);
-	        });
-	    });
-	});
-
-	function onScroll(event){
-	    var scrollPos = $(document).scrollTop();
-	    $('.nav a').each(function () {
-	        var currLink = $(this);
-	        var refElement = $(currLink.attr("href"));
-	        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-	            $('.nav ul li a').removeClass("active");
-	            currLink.addClass("active");
-	        }
-	        else{
-	            currLink.removeClass("active");
-	        }
-	    });
-	}
-
-
-	// Page loading animation
-	 $(window).on('load', function() {
-
-        $('#js-preloader').addClass('loaded');
-
+                var target = this.hash,
+                    menu = target;
+                var target = $(this.hash);
+                $('html, body')
+                    .stop()
+                    .animate(
+                        {
+                            scrollTop: target.offset().top + 1,
+                        },
+                        500,
+                        'swing',
+                        function () {
+                            window.location.hash = target;
+                            $(document).on('scroll', onScroll);
+                        }
+                    );
+            }
+        );
     });
 
+    function onScroll(event) {
+        var scrollPos = $(document).scrollTop();
+        $('.nav a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr('href'));
+            if (
+                refElement.position().top <= scrollPos &&
+                refElement.position().top + refElement.height() > scrollPos
+            ) {
+                $('.nav ul li a').removeClass('active');
+                currLink.addClass('active');
+            } else {
+                currLink.removeClass('active');
+            }
+        });
+    }
 
-	// Window Resize Mobile Menu Fix
-	$(window).on('resize', function() {
-		mobileNav();
-	});
+    // Page loading animation
+    $(window).on('load', function () {
+        $('#js-preloader').addClass('loaded');
+    });
 
+    // Window Resize Mobile Menu Fix
+    $(window).on('resize', function () {
+        mobileNav();
+    });
 
-	// Window Resize Mobile Menu Fix
-	function mobileNav() {
-		var width = $(window).width();
-		$('.submenu').on('click', function() {
-			if(width < 767) {
-				$('.submenu ul').removeClass('active');
-				$(this).find('ul').toggleClass('active');
-			}
-		});
-	}
-
-
+    // Window Resize Mobile Menu Fix
+    function mobileNav() {
+        var width = $(window).width();
+        $('.submenu').on('click', function () {
+            if (width < 767) {
+                $('.submenu ul').removeClass('active');
+                $(this).find('ul').toggleClass('active');
+            }
+        });
+    }
 })(window.jQuery);
